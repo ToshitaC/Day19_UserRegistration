0ffb7f73a0d81e09100c8d4ab2b52bb02bb815aa
ToshitaC <toshitachaudhari17@gmail.com>
2021-09-14 21:57:49 +0530

    Added:Email ID validation

--- UserRegistration.java
+++ UserRegistration.java
@@ -5,29 +5,23 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class UserRegistration {
-    public static void validateName(String firstName) {
-        String NamePattern = "^[A-Z]{1}[a-zA-z]{2,}";
-        Pattern pattern = Pattern.compile(NamePattern);
-        Matcher matcher = pattern.matcher(firstName);
-        if (matcher.find())
-            System.out.println(firstName + " is valid");
-        else {
-            System.out.println("First name starts with Capital letter and has minimum 3 characters.");
+    public static void validateEmail(String Email) {
+        String emailPattern = "^[a-zA-Z0-9]+([._+-][0-9a-zA-Z]+)*@[a-zA-Z0-9]+.[a-zA-Z]{2,4}([.][a-zA-Z]{2,4})?$"; // Pattern for Email
+        Pattern pattern = Pattern.compile(emailPattern);
+        Matcher match = pattern.matcher(Email);
+        if (match.find()) {
+            System.out.println("Email is Valid");
+        } else {
+            System.out.println("Email is invalid");
         }
     }
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String firstName = "";
-        String lastName = "";
-
-        System.out.println("Enter Firstname: ");
-        firstName = sc.nextLine();
-        validateName(firstName); //firstName method
-
-        System.out.println("Enter Lastname: ");
-        lastName = sc.nextLine();
-        validateName(lastName); //lastName method
+        String email = "";
+        System.out.println("Enter email Id: ");
+        email = sc.nextLine();
+        validateEmail(email); //email method
         sc.close();
     }
 
