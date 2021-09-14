ea058d476040390f011e4943e32c653c1816a037
ToshitaC <toshitachaudhari17@gmail.com>
2021-09-14 22:05:47 +0530

    Added:Mobile Number Validation

--- UserRegistration.java
+++ UserRegistration.java
@@ -5,23 +5,23 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class UserRegistration {
-    public static void validateEmail(String Email) {
-        String emailPattern = "^[a-zA-Z0-9]+([._+-][0-9a-zA-Z]+)*@[a-zA-Z0-9]+.[a-zA-Z]{2,4}([.][a-zA-Z]{2,4})?$"; // Pattern for Email
-        Pattern pattern = Pattern.compile(emailPattern);
-        Matcher match = pattern.matcher(Email);
-        if (match.find()) {
-            System.out.println("Email is Valid");
-        } else {
-            System.out.println("Email is invalid");
-        }
+    public static void validateMobile(String mobileNumber) {
+        String mobilePattern = "^[1-9]+[0-9]+[/s\\-]?[1-9]{1}[0-9]{9}$";
+        Pattern pattern = Pattern.compile(mobilePattern);
+        Matcher matcher = pattern.matcher(mobileNumber);
+
+        if (matcher.find())
+            System.out.println(mobileNumber + " is valid");
+        else
+            System.out.println(mobileNumber + " is not valid");
     }
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String email = "";
-        System.out.println("Enter email Id: ");
-        email = sc.nextLine();
-        validateEmail(email); //email method
+        String mobileNumber = "";
+        System.out.println("Enter the Mobile Number");
+        mobileNumber = sc.nextLine();
+        validateMobile(mobileNumber);//validation of MobileNumber
         sc.close();
     }
 
